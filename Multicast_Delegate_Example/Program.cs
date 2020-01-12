b75795096abf15401eea96c3ddb414c662c63c70
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-11 20:42:08 -0500

    Multicast_Delegate_Project created

---
+++ Multicast_Delegate_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multicast_Delegate_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
