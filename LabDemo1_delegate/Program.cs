b3e350dd7b2af053072566d2b5f691b91da7102e
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-11 20:13:27 -0500

    Add project files.

---
+++ LabDemo1_delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabDemo1_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
