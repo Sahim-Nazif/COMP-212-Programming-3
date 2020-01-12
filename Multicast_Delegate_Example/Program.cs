8d227a1335b46b6d7c90073b5b3fd148f32c011a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-11 20:45:09 -0500

    delclared a delegate with two parameters

--- Multicast_Delegate_Example/Program.cs
+++ Multicast_Delegate_Example/Program.cs
@@ -4,9 +4,16 @@ namespace Multicast_Delegate_Example
 {
     class Program
     {
+        public delegate double PerformCalculation(double x, double y);
+
+        public static double Add( double a, double b)
+        {
+            Console.WriteLine("The result of addition " + Math.Round(a + b));
+            return (a + b);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
