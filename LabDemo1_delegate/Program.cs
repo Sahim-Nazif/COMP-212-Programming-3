7c0070fbe77b78dfbdf98911004b2953dcc0a8c3
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-11 20:19:05 -0500

    methods declared

--- LabDemo1_delegate/Program.cs
+++ LabDemo1_delegate/Program.cs
@@ -3,9 +3,18 @@
 namespace LabDemo1_delegate
 {
     //delegate declaration
-    delegate void GreetingDelegate(string s);
+    delegate void GreetingDelegate(string name);
     class Program
     {
+        //method declaration
+        public static void Hello(string name)
+        {
+            Console.WriteLine("Hello, {0}", name);
+        }
+        public static void Goodbye(string name)
+        {
+            Console.WriteLine("Bye, {0}", name);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
