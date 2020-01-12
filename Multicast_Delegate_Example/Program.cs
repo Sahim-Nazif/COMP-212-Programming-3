90f7ea97576880170ac9fc8e4259975970f02134
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-11 20:53:49 -0500

    multicast delegate instantiated

--- Multicast_Delegate_Example/Program.cs
+++ Multicast_Delegate_Example/Program.cs
@@ -20,6 +20,21 @@ namespace Multicast_Delegate_Example
         {
             PerformCalculation addition = Add;
             addition(22.44, 32.89);
+            PerformCalculation division = Divide;
+            division(55.33, 22.30);
+
+            //instead of doing the above we can use Multi Cast delegate, i.e
+            PerformCalculation multiCal = addition + division;
+
+            multiCal(44.23, 22.31);
+
+            //we can also do the following 
+            multiCal -=division;
+            multiCal += addition;
+
+
+
+
 
             
 
