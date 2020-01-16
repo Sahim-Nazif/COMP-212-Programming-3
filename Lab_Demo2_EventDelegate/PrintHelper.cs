092664724df49bd0a4bd08381cf11f2e80db41d9
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-15 22:07:34 -0500

    added class - declaring delegate and event

---
+++ Lab_Demo2_EventDelegate/PrintHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Demo2_EventDelegate
+{
+    //this is the publisher class
+    public class PrintHelper
+    {
+        //declaring delegate
+        public delegate void BeforePrint();
+        //declaring event of type delegate (BeforePrint)
+
+        public event BeforePrint beforePrintEvent;
+
+    }
+}
